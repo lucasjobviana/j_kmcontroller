6c0e4fda9dbb9420f14fa15dc44ecf7cdcddefcf
Lucas Job Viana <lucasjobvianna@gmail.com>
2023-10-06 00:11:29 -0300

    :recycle: refactor: Excluindo componentes não utilizados.

--- frontend_jkc/src/shared/components/form/auto-complete-field/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './J_AutoCompleteField';
--- frontend_jkc/src/shared/components/form/index.ts
+++ frontend_jkc/src/shared/components/form/index.ts
@@ -1,5 +1,3 @@
 export * from './text-field';
 export * from './FormVehicleDetail';
-export * from './FormPostDetail';
 export * from './FormPlaceDetail';
-export * from './auto-complete-field';
