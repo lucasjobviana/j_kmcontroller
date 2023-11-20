246fab4e84e3f6df4937041aee3eed8a2f1ef49a
Lucas Job Viana <lucasjobvianna@gmail.com>
2023-11-19 23:35:45 -0300

    :refactor: fix: Eliminando dependencia interna.

--- backend_jkc/src/models/MaintenanceModel.ts
+++ backend_jkc/src/models/MaintenanceModel.ts
@@ -10,7 +10,7 @@ export default class MaintenanceModel extends BaseModel<TMaintenance>{
   ) { super(SequelizeMaintenanceModel,['id','initialDate','endDate','workshopId','vehicleId','description']);  }
 
   public async findAllLikeByFieldName(fieldName='name', searchValue = '',  fields = this.propNames): Promise<TMaintenance[]> {
-    const a = await SequelizeMaintenanceModel.findAll({
+    const a = await this.model.findAll({
       where: {
         [fieldName]: {
           [Op.like]: `%${searchValue}%`,
@@ -25,29 +25,32 @@ export default class MaintenanceModel extends BaseModel<TMaintenance>{
 
   public async update(id:string, obj:TMaintenance): Promise<TMaintenance> {
     console.log('model update by maintenance:/n\n', obj, '\n\n');
+    
     const services = await SequelizeServiceTaskModel.findAll(
       { where: { id: obj.services?.map((s)=>s.id) } },
     );
-
+     
     if(services.length !== obj.services?.length) {
       throw new Error('One or more Service not found');
     }
 
-    const transaction = await SequelizeMaintenanceModel.sequelize?.transaction();
+    const transaction = await this.model.sequelize?.transaction();
     try {
-      // const data = obj.services.map((s)=>s.dataValues);
-      const data = obj.services;
-      const description = obj.maintenance_service_association?.map((s)=>s.description);
-      const totalPrice = obj.maintenance_service_association?.map((s)=>s.totalPrice);
+      const services = obj.maintenance_service_association;
       await this.model.update(obj, {where: {id}}, { transaction:transaction });
       await MaintenanceServiceAssociation.destroy({ where: { maintenanceId: id }, transaction: transaction });
-      await MaintenanceServiceAssociation.bulkCreate(
-        data.map((s,index)=>({maintenanceId: Number(id), serviceId: s.id, totalPrice: totalPrice?.[index], description: description?.[index]})),
-        { transaction:transaction }
+      services && await MaintenanceServiceAssociation.bulkCreate(
+        services?.map((s) => ({
+          maintenanceId: Number(id),
+          serviceId: s.serviceId,
+          totalPrice: s.totalPrice,
+          description: s.description,
+        })), { transaction: transaction },
       );
 
+
       await transaction?.commit();
-      return obj;
+      return obj;'';
     } catch (error) {
       await transaction?.rollback(); throw error;
     }
